6aece70347ce0e5768aab1129b784296876a58c5
nyominkhat <nyominkhat329@gmail.com>
2024-12-20 21:18:33 +0630

    feat: project setup

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,28 +1,25 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: "Oops!" }} />
+
+      <Text>404 Page Not Found!</Text>
+
+      <Link href={"/"} style={styles.link}>
+        Go Back Home
+      </Link>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   link: {
---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+const App = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-3xl font-bold'>App</Text>
+      <Link href={"/profile"}>Go to Profile</Link>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
